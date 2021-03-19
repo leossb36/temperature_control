b40f7a022053ecf67c41ee235194ef59d1018844
Leonardo Barreiros <leossb36@gmail.com>
2021-03-19 19:08:08 -0300

    Corrigindo sintaxe

--- src/gpio.c
+++ src/gpio.c
@@ -6,6 +6,10 @@
 #include <wiringPi.h> /* include wiringPi library */
 #include <stdio.h>
 #include <softPwm.h>  /* include header file for software PWM */
+#include <unistd.h>
+
+#define DEVICE_FAN 5
+#define DEVICE_RESISTOR 4
 
 void handle_device(int PWM_pin, int intensity){
 
@@ -13,15 +17,15 @@ void handle_device(int PWM_pin, int intensity){
 	pinMode(PWM_pin,OUTPUT);	/* set GPIO as output */
 	softPwmCreate(PWM_pin,1,100);	/* set PWM channel along with range*/
 
-	softPwmWrite (PWM_pin, intensity);
+	softPwmWrite(PWM_pin, intensity);
 	usleep(10000);
 }
 
 
 void using_fan(int intensity) {
-	handle_device(5, intensity);
+	handle_device(DEVICE_FAN, intensity);
 }
 
 void using_resistor(int intensity) {
-	handle_device(4, intensity);
+	handle_device(DEVICE_RESISTOR, intensity);
 }
