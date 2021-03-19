bfa962b680fc127311688dc5f9b50ef123a07b35
Leonardo Barreiros <leossb36@gmail.com>
2021-03-19 16:11:39 -0300

    Ajustando main para realizar operaçoes

--- src/main.c
+++ src/main.c
@@ -1,107 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <string.h>
-#include <unistd.h>         //Used for UART
-#include <fcntl.h>          //Used for UART
-#include <termios.h>        //Used for UART
 #include "crc16.h"
+#include "uart.h"
+#include "i2clcd.h"
+#include "bme280_sensor.h"
 
 
-void stop_process(int signal) {
-    printf("Programa interrompido!\nFinalizando processos...\n");
+void cancelProcessUart(int signal) {
+    closeUart();
+    close_bme();
     exit(0);
 }
 
-int main(int argc, const char * argv[]) {
+int main(void) {
 
-    signal(SIGINT, stop_process);
-    signal(SIGKILL, stop_process);
+    signal(SIGINT, cancelProcessUart);
 
-    int uart0_filestream = -1;
+    float ti, tr, te;
 
-    uart0_filestream = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);      //Open in non blocking read/write mode
-    if (uart0_filestream == -1)
-    {
-        printf("Erro - Não foi possível iniciar a UART.\n");
+    char line_1[20], line_2[20];
+
+    setupLCD();
+    init_bme();
+
+    while(1) {
+        ti = get_temperature(INTERNAL_TEMPERATURE);
+        tr = get_temperature(REFERENCIAL_TEMPERATURE);
+        te = get_temperature_bme();
+
+        sprintf(line_1, "TI:%.2f TR:%.2f", ti, tr);
+        sprintf(line_2, "TE:%.2f ::", te);
+
+        writeOnLCD(line_1, line_2);
+
+        sleep(1);
     }
-    else
-    {
-        printf("UART inicializada!\n");
-    }
-
-    struct termios options;
-
-    tcgetattr(uart0_filestream, &options);
-    options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;     //<Set baud rate
-    options.c_iflag = IGNPAR;
-    options.c_oflag = 0;
-    options.c_lflag = 0;
-    tcflush(uart0_filestream, TCIFLUSH);
-    tcsetattr(uart0_filestream, TCSANOW, &options);
-
-    unsigned char tx_buffer[20];
-    unsigned char *p_tx_buffer;
-
-    p_tx_buffer = &tx_buffer[0];
-
-    *p_tx_buffer++ = 0x01;
-    *p_tx_buffer++ = 0x23;
-    *p_tx_buffer++ = 0xC1;
-    *p_tx_buffer++ = 0x05;
-    *p_tx_buffer++ = 0x05;
-    *p_tx_buffer++ = 0x02;
-    *p_tx_buffer++ = 0x01;
-
-    short crc16 = calcula_CRC(tx_buffer, (p_tx_buffer - &tx_buffer[0]));
-
-    *p_tx_buffer++ = crc16;
-    *p_tx_buffer++ = crc16 >> 8;
-
-    printf("Buffers de memória criados!\n");
-
-    if (uart0_filestream != -1)
-    {
-        printf("Escrevendo caracteres na UART ...");
-        int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
-        if (count < 0)
-        {
-            printf("UART TX error\n");
-        }
-        else
-        {
-            printf("escrito.\n");
-        }
-    }
-
-    sleep(1);
-
-    //----- CHECK FOR ANY RX BYTES -----
-    if (uart0_filestream != -1)
-    {
-        // Read up to 255 characters from the port if they are there
-        unsigned char rx_buffer[256];
-        int rx_length = read(uart0_filestream, (void*)rx_buffer , 255);      //Filestream, buffer to store in, number of bytes to read (max)
-        if (rx_length < 0)
-        {
-            printf("Erro na leitura.\n"); //An error occured (will occur if there are no bytes)
-        }
-        else if (rx_length == 0)
-        {
-            printf("Nenhum dado disponível.\n"); //No data waiting
-        }
-        else
-        {
-            //Bytes received
-            float temperatura;
-
-            memcpy(&temperatura, rx_buffer + 3, 4);
-
-            rx_buffer[rx_length] = '\0';
-            printf("%i Bytes lidos : %f\n", rx_length, temperatura);
-        }
-    }
-
-    close(uart0_filestream);
-   return 0;
+    return 0;
 }
