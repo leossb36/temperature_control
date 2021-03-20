42c930a7c1450bdeba7d88793929f3bddc84ac1c
Leonardo Barreiros <leossb36@gmail.com>
2021-03-20 00:24:24 -0300

    Configurando menu

--- src/menu.c
+++ src/menu.c
@@ -1,24 +1,78 @@
 #include <curses.h>
+#include <string.h>
+#include <unistd.h>
 
 void menu() {
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
+    int xMax, yMax, new_x, new_y;
+    int commands_box_size = 10;
+    int key = ERR;
 
-    WINDOW *main = newwin(6, xMax-12, yMax-8, 5);
-    box(main, 0, 0);
-    refresh();
-    keypad(main, true);
-}
+    int hightlight = 0;
 
-int main(int argc, char *argv[]) {
     initscr();
     noecho();
-    cbreak();
+    curs_set(FALSE);
+    keypad(stdscr, TRUE);
 
-    menu();
+    char options[2][30] = {"[1] - Temperatura Automática", "[2] - Temperatura Manual"};
 
-    getch();
+    // set up initial windows
+    getmaxyx(stdscr, yMax, xMax);
+
+    WINDOW *data_box = newwin(yMax - commands_box_size, xMax, 0, 0);
+    WINDOW *command_box = newwin(commands_box_size, xMax, yMax - commands_box_size, 0);
+
+    box(data_box, 0, 0);
+    box(command_box, 0, 0);
+
+    while(1) {
+
+        for(int i = 0; i < 2; i++) {
+            if (i == hightlight) {
+                wattron(command_box, A_REVERSE);
+            }
+            mvwprintw(command_box, 2, i+i*30 + 2, options[i]);
+            wattroff(command_box, A_REVERSE);
+        }
+        switch(key) {
+            case KEY_LEFT:
+                hightlight = 0;
+                break;
+            case KEY_RIGHT:
+                hightlight = 1;
+                break;
+            default:
+                break;
+        }
+
+        getmaxyx(stdscr, new_y, new_x);
+
+        if (new_y != yMax || new_x != xMax) {
+            xMax = new_x;
+            yMax = new_y;
+
+            wresize(data_box, new_y - commands_box_size, new_x);
+            wresize(command_box, commands_box_size, new_x);
+            mvwin(command_box, new_y - commands_box_size, 0);
+
+            wclear(stdscr);
+            wclear(data_box);
+            wclear(command_box);
+
+            box(data_box, 0, 0);
+            box(command_box, 0, 0);
+        }
+        // draw to our windows
+        mvwprintw(data_box, 0, 1, "Dados");
+        mvwprintw(command_box, 0, 1, "Comandos");
+
+        // refresh each window
+        wrefresh(stdscr);
+        wrefresh(data_box);
+        wrefresh(command_box);
+
+        key = getch();
+        usleep(10000);
+    }
     endwin();
-
-    return 0;
 }
