03a0b799936cb0647f5fe9a2f421b737119ea3eb
Leonardo Barreiros <leossb36@gmail.com>
2021-03-21 01:31:35 -0300

    Criando thread para execução paralela de menu

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <pthread.h>
 #include "crc16.h"
 #include "uart.h"
 #include "i2clcd.h"
@@ -8,68 +9,36 @@
 #include "gpio.h"
 #include "csv.h"
 #include "bme280_sensor.h"
+#include "control.h"
+#include "menu.h"
+#include <curses.h>
 
+pthread_t tid;
 
 void cancelProcessUart(int signal) {
     using_fan(0);
     using_resistor(0);
     closeUart();
     close_bme();
+    endwin();
     exit(0);
 }
 
-int main(void) {
+int main(int argc, char **argv) {
 
     signal(SIGINT, cancelProcessUart);
 
-    float ti, tr, te;
-    double kp, ki, kd;
-    kp = 5.0;
-    ki = 1.0;
-    kd = 5.0;
-    float pid = 0.0;
-    float resistor, fan;
+    pthread_create(&tid, NULL, &menu_execution, NULL);
 
-    char line_1[20], line_2[20];
+    pthread_detach(tid);
 
     setupLCD();
+
     init_bme();
-    pid_configura_constantes(kp, ki, kd);
+    pid_configura_constantes(5.0, 1.0, 5.0);
 
     while(1) {
-        ti = get_temperature(INTERNAL_TEMPERATURE);
-        tr = get_temperature(REFERENCIAL_TEMPERATURE);
-        te = get_temperature_bme();
-
-        pid_atualiza_referencia(tr);
-        pid = pid_controle(ti);
-
-        if (pid > 0) {
-            using_resistor(pid);
-            using_fan(0);
-            resistor = pid;
-            fan = 0;
-        }
-        else {
-            if(pid <= -40) {
-                using_fan(-1 * pid);
-                using_resistor(0);
-                fan = -1*pid;
-                resistor = 0;
-            }
-            else if (pid <= 0 && pid >= -40){
-                using_resistor(0);
-                using_fan(0);
-                fan = 0;
-                resistor = 0;
-            }
-        }
-
-        sprintf(line_1, "TI:%.2f TR:%.2f", ti, tr);
-        sprintf(line_2, "TE:%.2f", te);
-        writeOnLCD(line_1, line_2);
-        writeOnCSVFile(ti, tr, te, resistor, fan);
-        usleep(1000000);
+        update_display();
     }
 
     return 0;
